7c70e8d8190cc028366d954b3ac2f6e16b09b803
NekroCode <nekrojunk@hotmail.com>
2019-11-21 20:36:38 +0100

    Added about MenuItem functionality

---
+++ src/nekrocode/chessgame/userinterface/menubar/MyMenuBar.java
@@ -0,0 +1,45 @@
+package nekrocode.chessgame.userinterface.menubar;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextPane;
+
+// TODO Improve this class and its code only when there's time left for less important things
+public class MyMenuBar extends JMenuBar {
+
+	private static final long serialVersionUID = 1L;
+	
+	public MyMenuBar(JFrame frame) {
+		JMenu helpMenu = new JMenu("Help");
+		JMenuItem about = new JMenuItem("About");
+		about.addActionListener(o -> drawDialogBox(frame));
+		helpMenu.add(about);
+		add(helpMenu);
+	}
+	
+	// TODO Replace the resource heavy JTextPane for the less resource limiting JTextArea
+	// TODO Better placement of JDialog
+	private void drawDialogBox(JFrame frame) {
+		JDialog dialog = new JDialog(frame, "About", true);
+		JTextPane p = new JTextPane();
+		p.setEditable(false);
+		p.setText("My first attempt in building a chess application. \n"
+				+ "Version 0.1 \n\n"
+				+ "Special thanks to: \n"
+				+ "- Bill Harvey for the chess puzzles (http://wtharvey.com). \n"
+				+ "- Everyone involved for taking the time to help me improve! \n\n"
+				+ "Images used: \n"
+				+ "- https://commons.wikimedia.org/wiki/Category:SVG_chess_pieces \n\n"
+				+ "Improvement notes for version 0.2: \n"
+				+ "- Better abstraction between the backend, backend conversion and the graphical user interface \n"
+				+ "- Improve existing code by making use of interfaces where posssible");
+		dialog.add(p);
+		dialog.pack();
+		dialog.setLocation(frame.getLocation());
+		dialog.setVisible(true);
+	}
+
+}
--- src/nekrocode/chessgame/userinterface/util/GuiBuilder.java
+++ src/nekrocode/chessgame/userinterface/util/GuiBuilder.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.UIManager;
 
 import nekrocode.chessgame.test_environment.TestPanel;
+import nekrocode.chessgame.userinterface.menubar.MyMenuBar;
 
 /**
  * A separate class for drawing the graphical user interface is made so the non-static method
@@ -24,12 +25,12 @@ public class GuiBuilder {
 		// Don't forget to also change the image and its dimension
 		//try { frame.setIconImage(new ImageIcon(getClass().getClassLoader().getResource("imgs/icons/app_icon.png")).getImage()); } 
 			//catch (NullPointerException e) {}
-		//frame.setJMenuBar(new MyMenuBar());
+		frame.setJMenuBar(new MyMenuBar(frame));
 		//frame.setContentPane(new MainMenuPanel());
 		frame.setContentPane(new TestPanel());
-		//frame.pack();
-		//frame.setVisible(true);
-		System.exit(0);
+		frame.pack();
+		frame.setVisible(true);
+		//System.exit(0);
 	}
 	
 }
